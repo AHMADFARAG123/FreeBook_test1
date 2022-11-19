0fb023c97699fb58f0a46e4f8aed5faa1305bf5c
AHMADFARAG123 <ahmadfarag390@gmail.com>
2022-11-19 22:02:36 +0200

    Add project files.

---
+++ WebBook_test1/Areas/Admin/Controllers/AcountsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebBook_test1.Areas.Admin.Controllers
+{
+    public class AcountsController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebBook_test1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebBook_test1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
